b0f95c9a795119c890611202cf3b038221e1791e
yizhiqin809 <ayizhiqin@163.com>
2025-04-22 19:12:27 +0800

    Update regions.go

--- dataset/lib/regions.go
+++ dataset/lib/regions.go
@@ -11,7 +11,11 @@ const ZOOM = 18
 
 var regionMap map[string][4]float64 = map[string][4]float64{
 	
-	"jinghua":[4]float64{29.089369,119.636916,29.068396, 119.667353},
+	"hangzhou":[4]float64{30.273914, 120.170362,30.253914, 120.200362},
+	"xiaoshan":[4]float64{30.185474, 120.265899,30.165474, 120.295899},
+	"beijing":[4]float64{39.905250, 116.397281,39.885250, 116.427281},
+	
+	
 }
 
 type Region struct {
@@ -32,7 +36,7 @@ func GetRegions() []Region {
 		extreme := googlemaps.LonLatToPixel(common.Point{array[1], array[0]}, centerGPS, ZOOM)
 		radiusX := int(math.Ceil(math.Abs(extreme.X) / 4096))
 		radiusY := int(math.Ceil(math.Abs(extreme.Y) / 4096))
-		if name == "denver" || name == "kansas city" || name == "jinghua" || name == "san diego" || name == "pittsburgh" || name == "montreal" || name == "vancouver" || name == "tokyo" || name == "saltlakecity" || name == "paris" || name == "amsterdam" {
+		if name == "beijing" || name == "xiaoshan" || name == "jinghua" || name == "san diego" || name == "pittsburgh" || name == "montreal" || name == "vancouver" || name == "tokyo" || name == "saltlakecity" || name == "paris" || name == "amsterdam" {
 			radiusX = 1
 			radiusY = 1
 		}
